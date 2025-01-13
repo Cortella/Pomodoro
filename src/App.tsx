7d7e1e9289b39531ca961473ec7fd127b2686eac
Cortella <nicolau.cortella@gmail.com>
2025-01-12 21:16:32 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { Button } from "./components/Button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+    </>
+  );
+}
+
+export { App };
---
+++ src/components/Button.styles.ts
@@ -0,0 +1,19 @@
+import styled, { css } from "styled-components";
+
+export type ButtonVariant = "primary" | "secondary" | "success" | "danger";
+interface ButtoncontainerProps {
+  variant: ButtonVariant;
+}
+
+const buttonVariant = {
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  success: "green",
+};
+export const ButtonContainer = styled.button<ButtoncontainerProps>`
+  width: 100px;
+  height: 50px;
+
+  ${(props) => css`background-color: ${buttonVariant[props.variant]};`}
+`;
